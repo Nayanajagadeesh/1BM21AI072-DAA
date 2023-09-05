9563050368707202cdd5b58bb8e29be0d116e199
Nayana J <138456413+Nayanajagadeesh@users.noreply.github.com>
2023-09-05 08:58:56 +0530

    Update selectinaort.cpp

--- 1BM21AI072/selectinaort.cpp
+++ 1BM21AI072/selectinaort.cpp
@@ -1,35 +1,73 @@
-#include<iostream>
- using namespace std;
- int main()
- {
- int n,a[10],i,j,c,small,index,temp;
- cout<<"enter the size of the array";
- cin>>n;
- cout<<"enter the elements of the array";
- for(i=0;i<n;i++)
- cin>>a[i];
- for(i=0;i<n-1;i++)
- {
-     c=0;
-     small=a[i];
-     for(j=i+1;j<n;j++)
-     {
-         if(small>a[j])
-         {
-             small=a[j];
-             c++;
-             index=j;
-         }
-     }
-     if(c!=0)
-     {
-         temp=a[i];
-         a[i]=small;
-         a[index]=temp;
-     }
- }
- cout<<"\n sorted array is:\n";
- for(i=0;i<n;i++)
-    cout<<a[i]<<" ";
- return 0;
- }
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void selectionSort(int n, int arr[]) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+        if (min_index != i)
+            swap(arr[i], arr[min_index]);
+    }
+}
+
+int main() {
+    int n, ch;
+    clock_t start, end;
+
+    while (true) {
+        cout << "1: For manual entry of N value and array elements\n";
+        cout << "2: To display time taken for sorting number of elements N in the range 500 to 14500\n";
+        cout << "3: To exit\n";
+        cout << "Enter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+        case 1:
+            cout << "Enter the number of elements: ";
+            cin >> n;
+            int arr[15000];
+            cout << "Enter array elements: ";
+            for (int i = 0; i < n; i++)
+                cin >> arr[i];
+
+            start = clock();
+            selectionSort(n, arr);
+            end = clock();
+
+            cout << "Sorted array is: ";
+            for (int i = 0; i < n; i++)
+                cout << arr[i] << " ";
+
+            cout << "\nTime taken to sort " << n << " numbers is " << ((double)(end - start) / CLOCKS_PER_SEC) << " seconds\n";
+            break;
+
+        case 2:
+            n = 500;
+            while (n <= 14500) {
+                int arr[15000];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = n - i;
+                }
+                start = clock();
+                selectionSort(n, arr);
+                end = clock();
+
+                cout << "Time taken to sort " << n << " numbers is " << ((double)(end - start) / CLOCKS_PER_SEC) << " seconds\n";
+                n += 1000;
+            }
+            break;
+
+        case 3:
+            exit(0);
+
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+
+    return 0;
+}
