cdfa3a5c50de0fc80023072bfc8ffca179dfdd6f
Nayana J <138456413+Nayanajagadeesh@users.noreply.github.com>
2023-09-05 08:58:38 +0530

    Update quick sort.cpp

--- 1BM21AI072/quick sort.cpp
+++ 1BM21AI072/quick sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 void swap(int arr[] , int pos1, int pos2){
@@ -34,19 +35,61 @@ void quickSort(int arr[], int low, int high){
 	}
 }
 
-int main()
-{
-	int n ;
-	cout << " enter the size of array";
-	cin>>n;
-	int arr[n];
-	for( int i = 0 ; i < n; i++){
-		cin>> arr[i];
-	}
-	quickSort(arr, 0 , n-1);
-	cout<<"The sorted array is: ";
-	for( int i = 0 ; i < n; i++){
-		cout<< arr[i]<<"\t";
-	}
+int main() {
+    int n, ch;
+    clock_t start, end;
 
+    while (true) {
+        cout << "1: For manual entry of N value and array elements\n";
+        cout << "2: To display time taken for sorting number of elements N in the range 500 to 14500\n";
+        cout << "3: To exit\n";
+        cout << "Enter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+        case 1:
+            cout << "Enter the number of elements: ";
+            cin >> n;
+            int arr[15000];
+            cout << "Enter array elements: ";
+            for (int i = 0; i < n; i++)
+                cin >> arr[i];
+
+            start = clock();
+            quickSort(arr, 0, n - 1);
+            end = clock();
+
+            cout << "Sorted array is: ";
+            for (int i = 0; i < n; i++)
+                cout << arr[i] << " ";
+
+            cout << "\nTime taken to sort " << n << " numbers is " << ((double)(end - start) / CLOCKS_PER_SEC) << " seconds\n";
+            break;
+
+        case 2:
+            n = 500;
+            while (n <= 14500) {
+                int arr[15000];
+                for (int i = 0; i < n; i++) {
+                    // Using random number generator to fill the array
+                    arr[i] = rand() % 10000; // Generates random numbers in the range [0, 9999]
+                }
+                start = clock();
+                quickSort(arr, 0, n - 1);
+                end = clock();
+
+                cout << "Time taken to sort " << n << " numbers is " << ((double)(end - start) / CLOCKS_PER_SEC) << " seconds\n";
+                n += 1000;
+            }
+            break;
+
+        case 3:
+            exit(0);
+
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+
+    return 0;
 }
